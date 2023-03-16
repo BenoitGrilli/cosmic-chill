00b1944e193fb02f5fa4d2bd36c6698614f6958e
benwa06 <benwa06@hotmail.fr>
2023-03-16 12:53:01 +0100

    test 2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
-import "./styles/globals.css";
+import "./styles/output.css";
+import Navbar from "./components/Navbar";
+import Gallery from"./components/Gallery";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
@@ -15,12 +16,11 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <ThirdwebProvider activeChain={activeChain}>
-      <App />
+    <Navbar/>
+    <Gallery/>
+    <App />
     </ThirdwebProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
