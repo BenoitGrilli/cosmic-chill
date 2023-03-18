555ad4bc61f6c839cc9691abb510fbe4f6509e7c
benwa06 <benwa06@hotmail.fr>
2023-03-18 15:28:26 +0100

    Sauvegarde avant nouvelle version

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import { Routes, Route } from 'react-router-dom';
 import Gallery from './components/Gallery';
 import NavBar from './components/Navbar'
 import Home from "./pages/Home";
-
+import CountDown from "./components/Countdown";
+import Accueil from "./components/Accueil";
 function App() {
     return (
         <>
             <NavBar/>
 
+
             <Routes>
+                <Route path='/countdown' element={<CountDown seconds={100}/>} />
                 <Route path='/hero' element={<Hero/>} />
+                <Route path='/accueil' element={<Accueil/>} />
                 <Route path='/home' element={<Home/>} />
                 <Route path='/gallery' element={<Gallery/>} />
             </Routes>
