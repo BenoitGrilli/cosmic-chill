d0db5b612732b664b73ef8935d9183d370cfcc76
benwa06 <benwa06@hotmail.fr>
2023-03-18 15:46:58 +0100

    Loggign page
    added logging page
    removed nav bar in the logging page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 import React from "react";
-import Hero from './components/Hero';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Gallery from './components/Gallery';
-import NavBar from './components/Navbar'
 import Home from "./pages/Home";
 import CountDown from "./components/Countdown";
 import Accueil from "./components/Accueil";
+import LoggingPage from "./components/LoggingPage";
+import NavBar from './components/Navbar';
+import Hero from './components/Hero';
+
 function App() {
-    return (
-        <>
-            <NavBar/>
+  const location = useLocation();
+  const hideNavBar = location.pathname === '/loggingpage';
 
-
-            <Routes>
-                <Route path='/countdown' element={<CountDown seconds={100}/>} />
-                <Route path='/hero' element={<Hero/>} />
-                <Route path='/accueil' element={<Accueil/>} />
-                <Route path='/home' element={<Home/>} />
-                <Route path='/gallery' element={<Gallery/>} />
-            </Routes>
-            
-    
-        
-        
-        </>
-    );
+  return (
+    <>
+      {hideNavBar ? null : <NavBar />}
+      <Routes>
+        <Route path='/loggingpage' element={<LoggingPage/>} />
+        <Route path='/countdown' element={<CountDown seconds={100}/>} />
+        <Route path='/hero' element={<Hero/>} />
+        <Route path='/accueil' element={<Accueil/>} />
+        <Route path='/home' element={<Home/>} />
+        <Route path='/gallery' element={<Gallery/>} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
