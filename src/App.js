84ace71d7ef0bb73a3a1e1c9ff8c70b98a4b1310
benwa06 <benwa06@hotmail.fr>
2023-03-28 15:23:34 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Home from "./pages/Home";
 import SplashPage from "./pages/SplashPage";
 import NavBar from "./components/Navbar";
 
-
 import Error404 from "./pages/Error404";
 
 import MyCarousel from "./components/MyCarousel";
@@ -34,24 +33,21 @@ function App() {
   return (
     <>
       <ThirdwebProvider activeChain={Zhejiang}>
-       
         {/* {hideNavBar ? null : <NavBar />} */}
         <Routes>
-          <Route path="/" element={<SplashPage />} />
+          <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/mint" element={<MintPage />} />
 
           <Route path="/supply" element={<GetSupply />} />
-          <Route path="/gallery" element={<Gallery/>} />
+          <Route path="/gallery" element={<Gallery />} />
           <Route path="/mycollection" element={<YourCollection />} />
           <Route path="/mintbutton" element={<ButtonMint />} />
-
-
 
           <Route path="/description" element={<Description />} />
 
           <Route path="*" element={<Error404 />} />
-          
+
           <Route path="/astronaut" element={<Astronaut />} />
           <Route path="/faucet" element={<Faucet />} />
         </Routes>
