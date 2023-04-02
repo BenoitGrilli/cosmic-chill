8506c85bc964f8b62052011ad209845e8b28281c
benwa06 <benwa06@hotmail.fr>
2023-04-02 16:44:46 +0200

    Template

--- src/components/contractERC20.js
+++
@@ -1,1 +0,0 @@
-export const contractAddress = ('0xb3c620d9379eb50ad53e71C549bd1cfC0e796597');
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,9 @@
 import React from "react";
-import BuyButton from "../components/BuyButton";
-import Description from "../components/Description";
-import MyCarousel from "../components/MyCarousel";
-import "animate.css";
-import Typewriter from "typewriter-effect";
 import Navbar from "../components/Navbar";
-
-import Astronaut from "../components/Astronaut";
-import ButtonTemplate from "../components/ButtonTemplate";
-import WelcomeTyping from "../components/WelcomeTyping"
-import AstronautWelcome from "../components/AstronautWelcome";
-
 
 function Home() {
   const backgroundStyle = {
-    backgroundImage: "url('/images/bg-6.jpg')",
+    backgroundImage: "url('')",
     backgroundSize: "cover",
     backgroundPosition: "top",
     height: "h-screen",
@@ -22,30 +11,9 @@ function Home() {
 
   return (
     <>
-    <div>
-
-      <section className="text-gray-600 body-font" style={backgroundStyle}>
+    <div style={backgroundStyle}>
+      <section >
       <Navbar /> 
-      <WelcomeTyping/>
-
-      
-        <div class="pr-6 pl-6 pt-0 flex flex-col md:flex-row">
-          <div class="w-full md:w-1/2">
-            <Astronaut />
-            
-            <div className="flex justify-center">
-      <ButtonTemplate to="/gallery" text="COLLECTION"/>
-      </div>
-          </div>
-          <div>
-  
-            <div className=" mt-20 text-lg text-white">
-              <Description />
-            </div>
-          </div>
-        </div>
-       
-        <MyCarousel />
       </section>
       </div>
     </>
